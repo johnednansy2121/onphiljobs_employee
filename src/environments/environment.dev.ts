b96194dff1eed550a8757127f73810b0afc5c915
Jeff930 <jefferson30.jdg@gmail.com>
2020-09-08 00:23:03 +0800

    set environment  for local

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,6 +3,8 @@ export const environment = {
   name: 'Development',
   theme: 'green',
   initial_page: '/blog/news',
-  api_path: 'https://devapi.fllair.com/',
-  domain: 'dev.fllair.com'
+  // api_path: 'https://devapi.fllair.com/',
+  // domain: 'dev.fllair.com'
+  api_path: "http://localhost:3000/",
+  domain: 'localhost'
 };
